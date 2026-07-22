fe61ea0db22cd0a3391936d06618a3a9b77a0f2a
patriq128 <patrik128kamidra@gmail.com>
2026-07-22 09:40:08 +0200

    new

--- download/system/main.py
+++ download/system/main.py
@@ -1,4 +1,11 @@
-from kernel.boot import main
+from kernel.boot import main as main_boot
+
+def main():
+    while True:
+        try:
+            main_boot()
+        except:
+            main_boot()
 
 if __name__ == "__main__":
     main()
