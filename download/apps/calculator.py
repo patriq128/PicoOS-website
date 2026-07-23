d1d6cc6214156beef053eff11e1aecd32700cff9
patriq128 <patrik128kamidra@gmail.com>
2026-07-23 09:27:48 +0200

    calculator update

--- download/apps/calculator.py
+++ download/apps/calculator.py
@@ -1,9 +1,23 @@
 def install():
     return {
         "name": "calculator",
-        "version": "1.0",
+        "version": "1.1",
         "autor": "ZiDi"
     }
 
 def main():
-    print("test chill")
+    print("Super Calculator")
+    first = input("Type the first number: ")
+    symbol = input("Type the symbol: ")
+    second = input("Type the second number: ")
+    if symbol == "+":
+        print(int(first) + int(second))
+    elif symbol == "-":
+        print(int(first) - int(second))
+    elif symbol == "*":
+        print(int(first) * int(second))
+    elif symbol == "/":
+        print(int(first) / int(second))
+    else:
+        print("Wrong symbol")
+        print("Use +, -, *, /")
